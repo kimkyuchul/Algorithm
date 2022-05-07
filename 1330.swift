ca71067b56009c734d98bdef438b8fab76f914ed
kimkyuchul <rbcjf122@gmail.com>
2022-05-07 22:39:06 +0900

    1330 solved

---
+++ 1330.swift
@@ -0,0 +1,12 @@
+
+let valueArr = readLine()!.split(separator: " ").map({ (valueArr) -> Int in return Int(valueArr)! })
+let a = valueArr[0]
+let b = valueArr[1]
+
+if a > b {
+    print(">")
+} else if a < b {
+    print("<")
+} else {
+    print("==")
+}
