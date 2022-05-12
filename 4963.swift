72b744b4cb95bdb1e23ff4df4211e263b7ff8f39
kimkyuchul <rbcjf122@gmail.com>
2022-05-12 23:46:10 +0900

    Update 4963.swift

--- 4963.swift
+++ 4963.swift
@@ -3,3 +3,11 @@ import Foundation
 let a = Int(readLine()!)!
 let b = Int(readLine()!)!
 
+@IBAction func group(_ sender: UIButton) {
+    let group = DispatchGroup()
+    
+    DispatchQueue.global(qos: .background).async(group: group) {
+        for i in 1...50 {
+            print(i, terminator: " ")
+        }
+        print("\nBackground 완료")
