cf24199223badcece45976dcf6160d1bf60189fe
김규철 <rbcjf122@gmail.com>
2023-05-28 01:29:27 +0900

    구조 변경

---
+++ 프로그래머스/몫 구하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    var result = 0
+    
+    if num2 != 0 {
+        result = num1 / num2
+    }
+    
+    return result
+}
+
+
+
+
