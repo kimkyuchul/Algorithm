2a3c14552a19f629a5da7b8aca93e2c536f8de31
김규철 <rbcjf122@gmail.com>
2024-05-08 12:43:12 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    if n <= 7 {
+        return 1
+    } else if n % 7 == 0 {
+        return n / 7
+    } else {
+     return n / 7 + 1   
+    }
+}
