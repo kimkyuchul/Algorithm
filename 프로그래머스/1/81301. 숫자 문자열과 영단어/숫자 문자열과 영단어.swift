f36e671e7c2e763f7941ac5baa8b0c27bdaff00b
김규철 <rbcjf122@gmail.com>
2025-06-08 18:49:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.35 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -0,0 +1,15 @@
+import Foundation
+func solution(_ s: String) -> Int {
+    var result = s
+    
+    let values = [
+        ("zero", "0"), ("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"),
+        ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9")
+    ]
+    
+    for (key, value) in values {
+        result = result.replacingOccurrences(of: key, with: value)
+    }
+    
+    return Int(result)!
+}
