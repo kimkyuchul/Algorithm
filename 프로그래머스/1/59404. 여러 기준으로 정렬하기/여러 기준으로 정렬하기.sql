8f65569c604a05ea08ac9097cc30b790a12e896a
김규철 <rbcjf122@gmail.com>
2026-02-09 23:31:38 +0900

    [level 1] Title: 여러 기준으로 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
+++ 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
@@ -2,3 +2,10 @@
 SELECT ANIMAL_ID, NAME, DATETIME
 FROM ANIMAL_INS
 ORDER BY NAME ASC, DATETIME DESC
+
+/*
+먼저 NAME 기준으로 오름차순 정렬
+NAME이 같은 경우에만 DATETIME 기준으로 내림차순 정렬
+
+즉, 첫 번째 조건이 우선이고, 동일한 값이 있을 때 두 번째 조건이 적용되는 방식
+*/
