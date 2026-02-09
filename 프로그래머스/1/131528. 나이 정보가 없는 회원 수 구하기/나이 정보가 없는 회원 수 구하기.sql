23f455254dad9873ba97c71237ddc06a5db3a90f
김규철 <rbcjf122@gmail.com>
2026-02-09 23:21:43 +0900

    [level 1] Title: 나이 정보가 없는 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131528. 나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql
+++ 프로그래머스/1/131528. 나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql
@@ -2,3 +2,11 @@
 SELECT COUNT(*) AS USERS
 FROM USER_INFO
 WHERE AGE IS NULL;
+
+/*
+COUNT(*) → 조건에 맞는 모든 행 수 (NULL 포함)
+COUNT(USER_ID) → USER_ID가 NULL이 아닌 행만 카운트
+
+COUNT(컬럼명)은 해당 컬럼이 NULL인 행을 제외
+COUNT(컬럼명)은 해당 컬럼이 NULL인 행을 제외
+*/
