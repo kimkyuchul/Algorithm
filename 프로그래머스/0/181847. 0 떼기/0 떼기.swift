e73b5d3a6fa9a7a4d312690135113d1c939403e4
김규철 <rbcjf122@gmail.com>
2026-05-21 12:52:43 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 15.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ n_str:String) -> String {
+    var result = n_str
+    
+    for i in n_str {
+        if i != "0" {
+            break
+        }
+        
+        if i == "0" {
+            result.removeFirst()
+        }
+    }
+    
+    return result
+}
