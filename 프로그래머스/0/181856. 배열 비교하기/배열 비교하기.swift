ea903c3e5f74419f742ba5923874bbdde9816685
김규철 <rbcjf122@gmail.com>
2026-05-24 00:21:06 +0900

    [level 0] Title: 배열 비교하기, Time: 0.06 ms, Memory: 15.9 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.swift
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.swift
@@ -5,20 +5,9 @@ func solution(_ arr1:[Int], _ arr2:[Int]) -> Int {
         return arr1.count < arr2.count ? -1 : 1
     }
     
-    var a1 = 0 
-    var a2 = 0
-    
-    for i in arr1 {
-        a1 += i
-    }
-    
-    for i in arr2 {
-        a2 += i
-    }
-    
-    if a1 < a2 {
+    if arr1.reduce(0, +) < arr2.reduce(0, +) {
         return -1
-    } else if a1 > a2 {
+    } else if arr1.reduce(0, +) > arr2.reduce(0, +) {
         return 1
     } else {
         return 0
